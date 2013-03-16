a65efa8807da80d0767406e987b66df41f1c2fcd
Ian Murray <ian.c.murray@gmail.com>
2013-03-16 15:26:25 +0000

    Mongo DB deps

--- project/Build.scala
+++ project/Build.scala
@@ -8,14 +8,14 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "org.reactivemongo" %% "reactivemongo" % "0.8",
+    "play.modules.reactivemongo" %% "play2-reactivemongo" % "0.1-SNAPSHOT"  cross CrossVersion.full
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    resolvers += "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
   )
 
 }
