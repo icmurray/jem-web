f34367d3945312c251be876336de2102e081e4dc
Ian Murray <ian.c.murray@gmail.com>
2013-04-30 10:00:23 +0100

    Update play version to 2.1.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,17 +5,14 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "jem-web"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "0.1-SNAPSHOT"
 
   val appDependencies = Seq(
     "org.reactivemongo" %% "play2-reactivemongo" % "0.8"
-    //"play.modules.reactivemongo" %% "play2-reactivemongo" % "0.1-SNAPSHOT"  cross CrossVersion.full
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here
-    resolvers += "sgodbillon" at "https://bitbucket.org/sgodbillon/repository/raw/master/snapshots/"
   )
 
 }
