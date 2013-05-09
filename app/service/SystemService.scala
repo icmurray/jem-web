6677de88988dbffbb57543a3c59c445a2c3165f3
Ian Murray <ian.c.murray@gmail.com>
2013-05-09 11:07:13 +0100

    Read Recordings from the backend.

--- app/service/SystemService.scala
+++ app/service/SystemService.scala
@@ -6,11 +6,12 @@ import com.typesafe.config._
 
 import org.joda.time.DateTime
 
+import play.api.data.validation.ValidationError
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 import play.api.libs.ws.WS
 
-import domain.{SystemStatus, Device, Gateway, RecordedRunConfiguration, Recording}
+import domain.{SystemStatus, Device, Gateway, RecordedRunConfiguration, Recording, ConfiguredGateway, ConfiguredDevice}
 
 trait SystemService {
   def status(implicit ec: ExecutionContext): Future[SystemStatus]
@@ -31,7 +32,19 @@ object SystemService extends SystemService {
   private val startUrl   = backendUrl + "/system-control/start"
   private val stopUrl    = backendUrl + "/system-control/stop"
   private val devicesUrl = backendUrl + "/system-control/attached-devices"
+  private val recordingsUrl = backendUrl + "/system-control/recordings"
   private val stopRecordingUrl = backendUrl + "/system-control/recordings/"
+
+  implicit private val jodaDateReads = new Reads[DateTime] {
+
+    def reads(json: JsValue): JsResult[DateTime] = json match {
+      case JsNumber(d) => JsSuccess(new DateTime((d * 1000L).toLong))
+      case _           => {
+        JsError(Seq(JsPath() -> Seq(ValidationError(
+          "validate.error.expected.date"))))
+      }
+    }
+  }
 
   implicit private val deviceReads: Reads[Device] = (
     (__ \ "unit").read[Int] ~
@@ -46,45 +59,52 @@ object SystemService extends SystemService {
 
   /** Because the API doesn't provide top-level arrays */
   private case class GatewayAggregate(gateways: List[Gateway])
+  private case class RecordingAggregate(recordings: List[Recording])
 
   implicit private val aggGatewayReads: Reads[GatewayAggregate] = (
     (__ \ "gateways").read[List[Gateway]] ~
     (__ \ "gateways").read[List[Gateway]] // Workaround..
   )((gws, _) => GatewayAggregate(gws))
 
-  override def recordedRuns(implicit ec: ExecutionContext) = future {
-    List(
-      Recording(
-        id="518a6eb6a63728758313e9fd",
-        status="running",
-        startTime=new DateTime(2013, 5, 8, 16, 41, 22),
-        endTime=None,
-        configuredGateways=List()),
-      Recording(
-        id="518a6eb6a63728758313e9fc",
-        status="ended",
-        startTime=new DateTime(2013, 5, 7, 15, 32, 22),
-        endTime=Some(new DateTime(2013, 5, 8, 9, 50, 33)),
-        configuredGateways=List()),
-      Recording(
-        id="518a6eb6a63728758313e9fb",
-        status="ended",
-        startTime=new DateTime(2013, 5, 6, 14, 23, 22),
-        endTime=Some(new DateTime(2013, 5, 6, 15, 50, 33)),
-        configuredGateways=List()),
-      Recording(
-        id="518a6eb6a63728758313e9fa",
-        status="ended",
-        startTime=new DateTime(2013, 5, 5, 13, 15, 22),
-        endTime=Some(new DateTime(2013, 5, 6, 8, 50, 33)),
-        configuredGateways=List()),
-      Recording(
-        id="518a6eb6a63728758313e9f0",
-        status="ended",
-        startTime=new DateTime(2013, 5, 4, 12, 6, 22),
-        endTime=Some(new DateTime(2013, 5, 4, 15, 50, 33)),
-        configuredGateways=List())
-    )
+  implicit private val configuredDeviceReads: Reads[ConfiguredDevice] = (
+    (__ \ "unit").read[Int] ~
+    (__ \ "table_ids").read[List[Int]]
+  )((unit, ids) => ConfiguredDevice(
+    unit = unit,
+    table1 = ids.contains(1),
+    table2 = ids.contains(2),
+    table3 = ids.contains(3),
+    table4 = ids.contains(4),
+    table5 = ids.contains(5),
+    table6 = ids.contains(6)))
+
+  implicit private val configuredGatewayReads: Reads[ConfiguredGateway] = (
+    (__ \ "host").read[String] ~
+    (__ \ "port").read[Int] ~
+    (__ \ "configured_devices").read[List[ConfiguredDevice]]
+  )(ConfiguredGateway)
+
+  implicit private val recordingReads: Reads[Recording] = (
+    (__ \ "id").read[String] ~
+    (__ \ "status").read[String] ~
+    (__ \ "start_time").read[DateTime] ~
+    (__ \ "end_time").read[Option[DateTime]] ~
+    (__ \ "configured_gateways").read[List[ConfiguredGateway]]
+  )(Recording)
+
+  implicit private val aggRecordingReads: Reads[RecordingAggregate] = (
+    (__ \ "recordings").read[List[Recording]] ~
+    (__ \ "recordings").read[List[Recording]] // Workaround..
+  )((rs, _) => RecordingAggregate(rs))
+
+  override def recordedRuns(implicit ec: ExecutionContext) = {
+    WS.url(recordingsUrl).get().map { response =>
+      val aggregate = response.json.validate[RecordingAggregate]
+      aggregate.fold(
+        errors => throw new RuntimeException("Bad response"),
+        valid  => valid.recordings
+      )
+    }
   }
 
   override def stopRecordedRun(id: String)(implicit ec: ExecutionContext) = {
